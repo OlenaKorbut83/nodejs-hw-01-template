c81aecd81a18cffa21348b66aebda29db624a493
OlenaKorbut83 <olenakorbut83@gmail.com>
2025-03-26 23:48:26 +0200

    homework done

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,10 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const removeLastContact = async () => {
+  const contactsList = await readContacts();
+  contactsList.pop();
+  await writeContacts(contactsList);
+};
 
 removeLastContact();
--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,10 @@
+import fs from 'node:fs/promises';
+
 import { PATH_DB } from '../constants/contacts.js';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+  const readContactUtil = await fs.readFile(PATH_DB, 'utf-8');
+  return JSON.parse(readContactUtil);
+};
+
+readContacts();
